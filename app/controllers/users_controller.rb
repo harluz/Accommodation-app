72aaa2bccd3910e248ca96c15a4cfd197c0d183d
harluz <harluz06@gmail.com>
2023-02-12 17:37:57 +0900

    [update] アイコン画像のデフォルトを表示できるように機能修正したもの

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+  before_action :default_avatar
+
   def profile
     @user = current_user
+  end
+
+  protected
+
+  def default_avatar
+    if !current_user.avatar.attached?
+      current_user.avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default-avatar-image.png')), filename: 'default-avatar-image.png', content_type: 'image/png')
+    end
   end
 end
