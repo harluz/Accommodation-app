c622a24a9b3369950d1a946b3bcd0040e9d6aa89
harluz <harluz06@gmail.com>
2023-02-12 17:36:27 +0900

    [add] 「rails active_storage:install」コマンドを実行し、ActiveStorageの導入及びマイグレーション、モデル関連付けを行ったもの

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # :avatarはファイル名　なんでもいい
+  has_one_attached :avatar
+  
   validates :name, presence: true
   
   # Include default devise modules. Others available are:
