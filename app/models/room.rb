d72f82ece7e052fb9eb36813a4eca249ae7b9c52
harluz <harluz06@gmail.com>
2023-02-13 19:03:06 +0900

    [update] Roomモデルにバリデーションを追加及びActive_storageとの関連付けも仮実装したもの

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,9 @@
 class Room < ApplicationRecord
+  has_one_attached :room_image
+
+  validates :name, presence: true
+  validates :introduction, length: { maximum: 300}
+  validates :detail, presence: true, length: { maximum: 1000 }
+  validates :adress, presence: true
+  validates :price, presence: true
 end
